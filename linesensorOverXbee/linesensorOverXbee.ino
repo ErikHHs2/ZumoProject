05e0729b221d6b52a47bd4d5f84c8dbb75588c20
ErikHHs2 <22048642@student.hhs.nl>
2023-06-07 19:06:30 +0200

    linesSenor met harcode kleuren

--- linesensorOverXbee/linesensorOverXbee.ino
+++ linesensorOverXbee/linesensorOverXbee.ino
@@ -2,7 +2,8 @@
 #include <Zumo32U4.h>
 
 
-const uint16_t maxSpeed = 0;
+uint16_t maxSpeed = 200;
+uint16_t OGmaxSpeed = 200;
 
 Zumo32U4Buzzer buzzer;
 Zumo32U4LineSensors lineSensors;
@@ -15,6 +16,11 @@ int16_t lastError = 0;
 unsigned int lineSensorValues[NUM_SENSORS];
 bool useEmitters = true;
 
+int Z = 0;
+int B = 0;
+int G = 0;
+int Grijs_L = 0;
+int Grijs_R = 0;
 
 void calibrateSensors()
 {
@@ -35,7 +41,9 @@ void calibrateSensors()
   motors.setSpeeds(0, 0);
 }
 
+void rijdenMetKleuren(){
 
+}
 
 void setup()
 {
@@ -53,6 +61,7 @@ void setup()
 
   buzzer.play("L16 cdegreg4");
   while(buzzer.isPlaying());
+ 
 }
 
 void printReadingsToSerial()
@@ -71,6 +80,62 @@ void printReadingsToSerial()
 
 void loop()
 {
+  kleurenHerkenen();
+  // if (Z = 1) {
+  //   rijdenZwart();
+  // }else if (G = 1) {
+  //   	rijdenZwart();
+  // }
+  rijdenZwart();
+
+  
+}
+
+void kleurenHerkenen(){
+  if (500 < lineSensorValues[2] && lineSensorValues[2] < 1200 || 500 < lineSensorValues[1] && lineSensorValues[1] < 1200 || 500 < lineSensorValues[3] && lineSensorValues[3] < 1200)
+  {
+      // delay(500);
+      Serial1.print("zwart");
+      int Z = 1;
+      maxSpeed = OGmaxSpeed;
+  }else if (250 < lineSensorValues[2] && lineSensorValues[2] < 400)
+  {
+      // delay(500);
+      // Serial1.print("bruin");
+      int B = 1;
+  }else if (120 < lineSensorValues[2] && lineSensorValues[2] < 220)
+  {
+      delay(500);
+      Serial1.print("groen");
+      int G = 1;
+      // maxSpeed = maxSpeed*0.5;
+      }
+
+else if (250 < lineSensorValues[0] && lineSensorValues[0] < 400)
+  {
+      delay(500);
+      Serial1.print("grijs/bruin links");
+      int Grijs_L = 1;}
+else if (250 < lineSensorValues[4] && lineSensorValues[4] < 400)
+  {
+      delay(500);
+      Serial1.print("Grijs/bruin Rechts");
+      int Grijs_R = 1;};
+      /* dit staat uit voor teste zonder grijze tape*/
+  // }else if ( 450 < lineSensorValues[0] && lineSensorValues[0] < 550 )
+  // {
+  //     delay(500);
+  //     Serial1.print("grijs links");
+  //     int Grijs_L = 1;
+  // }else if (450 < lineSensorValues[4] && lineSensorValues[4] < 550)
+  // {
+  //     delay(500);
+  //     Serial1.print("Grijs Rechts");
+  //     int Grijs_R = 1;
+  // };
+}
+
+void rijdenZwart(){
   int16_t position = lineSensors.readLine(lineSensorValues);
 
   int16_t error = position - 2000;
@@ -99,30 +164,7 @@ void loop()
   }
 
   motors.setSpeeds(leftSpeed, rightSpeed);
-  
-    if (500 < lineSensorValues[2] && lineSensorValues[2] < 800)
-  {
-      delay(500);
-      Serial1.print("zwart");
-  }else if (250 < lineSensorValues[2] && lineSensorValues[2] < 400)
-  {
-      delay(500);
-      Serial1.print("bruin");
-  }else if (120 < lineSensorValues[2] && lineSensorValues[2] < 220)
-  {
-      delay(500);
-      Serial1.print("groen");
-  }else if ( 450 < lineSensorValues[0] && lineSensorValues[0] < 550 )
-  {
-      delay(500);
-      Serial1.print("grijs links");
-  }else if (450 < lineSensorValues[4] && lineSensorValues[4] < 550)
-  {
-      delay(500);
-      Serial1.print("Grijs Rechts");
-  };
 }
-
 
 
 
